a593778ff41cae875368e9558a5c10301b2c08a7
Ronald <ronaldme@gmail.com>
2019-10-26 13:22:35 +0200

    Add registration entity / repository

---
+++ CCCP.DAL/Entities/Registration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CCCP.DAL.Entities
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int SubmitCount { get; set; }
+    }
+}
---
+++ CCCP.DAL/Repositories/RegistrationRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CCCP.DAL.Entities;
+
+namespace CCCP.DAL.Repositories
+{
+    public class RegistrationRepository : RepositoryBase<Registration>
+    {
+        public IEnumerable<Registration> GetRegistrations(int userId)
+        {
+            using (var db = Create())
+                return Collection(db).Find(c => c.UserId == userId);
+        }
+
+        public Tuple<Registration, Registration> GetFirstAndLastRegistrations()
+        {
+            using (var db = Create())
+            {
+                var registrations = Collection(db).FindAll().ToList();
+                if (!registrations.Any()) return null;
+
+                var first = registrations.First();
+                var last = registrations.Last();
+
+                return new Tuple<Registration, Registration>(first, last);
+            }
+        }
+    }
+}
