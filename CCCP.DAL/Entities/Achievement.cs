2529551c81220c7e61d9cb4994bbd03051813f28
Ronald <ronaldme@gmail.com>
2020-01-07 20:30:28 +0100

    Add Achievement / AchievementRepository

---
+++ CCCP.DAL/Entities/Achievement.cs
@@ -0,0 +1,9 @@
+﻿namespace CCCP.DAL.Entities
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public int SavedCups { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ CCCP.DAL/Repositories/AchievementRepository.cs
@@ -0,0 +1,13 @@
+﻿using CCCP.DAL.Entities;
+
+namespace CCCP.DAL.Repositories
+{
+    public class AchievementRepository : RepositoryBase<Achievement>
+    {
+        public Achievement GetAchievement(int savedCups)
+        {
+            using var db = Create();
+            return Collection(db).FindOne(c => c.SavedCups == savedCups);
+        }
+    }
+}
